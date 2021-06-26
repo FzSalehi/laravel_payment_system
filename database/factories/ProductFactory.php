51e929ec925f4a4e642f4d57253cec550f462ca9
Faraz_Salehi <faraz67fz@gmail.com>
2021-06-26 16:07:39 +0430

    created product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -21,6 +21,7 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        Product::truncate();
         return [
             'title' => $this->faker->randomElement([
                 'موبایل سامسونگ',
@@ -39,11 +40,11 @@ class ProductFactory extends Factory
             ]),
             'description' => 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه .',
             'image' => 'https://via.placeholder.com/286x180?text=Image',
-            
+
             'price' => $this->faker->randomElement([
                 150000, 450000, 252000, 2521000, 250000, 150000, 850000, 650000, 450000, 950000, 410000, 320000
             ]),
-
+            
             'stock'=> $this->faker->randomDigitNotNull,
 
         ];
